f65582d8974dfea8b7cc0ab82316b4cb3436c2c1
Rajendra Pasha <105064715+Vtolin@users.noreply.github.com>
2025-01-23 16:30:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api', userRoutes);
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
